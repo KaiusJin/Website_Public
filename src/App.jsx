1f68441689cdd856490287ab3d7171fa36afe244
Kaius Jin <au190826@outlook.com>
2026-04-29 14:06:12 -0400

    fix: add missing useEffect import

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import NavBar from './components/NavBar';
 import About from './components/sections/About';
 import Projects from './components/sections/Projects';
